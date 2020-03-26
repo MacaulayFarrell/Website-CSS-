31212cdb257e29f6c39d824d47c84019e8de26da
MacaulayFarrell <46494320+MacaulayFarrell@users.noreply.github.com>
2020-03-26 23:17:34 +0000

    beautify

--- CTEC1800-Stylesheet.js
+++ CTEC1800-Stylesheet.js
@@ -1,23 +1,20 @@
+function setActiveStyle(styletitle) {
+  var stylelist = document.getElementsByTagName("link");
+  for (i = 0; i < stylelist.length; i++) {
+    if (isOptionalStylesheet(stylelist[i])) {
+      activateWhenMatching(stylelist[i], styletitle);
+    }
+  }
+}
 
+function isOptionalStylesheet(thislink) {
+  return (thislink.getAttribute("rel").indexOf("style") != -1) && thislink.getAttribute("title")
+}
 
-		function setActiveStyle (styletitle) {
-			var stylelist = document.getElementsByTagName("link");
-			for (i = 0; i < stylelist.length; i++) {
-				if (isOptionalStylesheet(stylelist[i])) {
-					activateWhenMatching(stylelist[i], styletitle);
-				}
-			}
-		}
-
-		function isOptionalStylesheet(thislink) {
-			return (thislink.getAttribute("rel").indexOf("style") != -1) && thislink.getAttribute("title")
-			}
-
-		function activateWhenMatching(thislink, styletitle) {
-			if (thislink.getAttribute("title") == styletitle) {
-				thislink.disabled = false;
-			}
-			else {
-				thislink.disabled = true;
-			}
-		}
+function activateWhenMatching(thislink, styletitle) {
+  if (thislink.getAttribute("title") == styletitle) {
+    thislink.disabled = false;
+  } else {
+    thislink.disabled = true;
+  }
+}
